12a8d0c5d105926450dbf37960c7d537c28c3a7b
Damith88 <damithcjk@gmail.com>
2015-06-07 15:33:08 +0530

    sms application code

---
+++ config/DatabaseConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+class DatabaseConfig {
+    const HOST = 'localhost';
+    const USER_NAME = 'root';
+    const PASSWORD = '123';
+    const DB_NAME = 'tad_courier';
+    const PORT = 3306;
+    const TEST_DB_NAME = 'test_tad_courier';
+}
