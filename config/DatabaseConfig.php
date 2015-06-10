a7c7ca542b9ab0d79a20b060c200645ca9fc1c51
Nuwan Chathuranga <shermal@Nuwan.(none)>
2015-06-10 17:13:46 +0530

    saving way points

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -3,7 +3,7 @@
 class DatabaseConfig {
     const HOST = 'localhost';
     const USER_NAME = 'root';
-    const PASSWORD = '123';
+    const PASSWORD = 'root';
     const DB_NAME = 'tad_courier';
     const PORT = 3306;
     const TEST_DB_NAME = 'test_tad_courier';
